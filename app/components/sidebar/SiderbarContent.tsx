ee2f05f31275e810c4c41fd4a5cd5d7d47677483
Daniel Prieto <prietogaviriadaniel@gmail.com>
2024-01-21 10:20:07 +0100

    feat: adding logo sidebar

--- app/components/sidebar/SiderbarContent.tsx
+++ app/components/sidebar/SiderbarContent.tsx
@@ -32,9 +32,10 @@ const SiderbarContent: React.FC<SiderbarContentProps> = ({ currentUser,conversat
         currentUser={currentUser}
       />
       <aside
-        className={`${isOpen ? 'hidden lg:flex' : 'flex'} bg-neutral-950 z-30 absolute w-full h-24 items-center justify-center gap-10 bottom-0 lg:py-12 lg:w-28 lg:h-full lg:left-0 lg:min-h-[520px] lg:border-r lg:border-neutral-700 lg:flex-col lg:justify-between 2xl:w-32`}
+        className={`${isOpen ? 'hidden lg:flex' : 'flex'} bg-neutral-950 z-30 absolute w-full h-24 items-center justify-center gap-10 bottom-0 lg:py-12 lg:w-28 lg:h-full lg:left-0 lg:min-h-[520px] lg:border-r lg:border-neutral-700 lg:flex-col lg:gap-0 2xl:w-32`}
       >
-        <nav>
+        <img className='w-auto h-8 invert hidden lg:mb-16 lg:block' src='/chatverse-icon.webp' alt='chatverse-icon' />
+        <nav className="lg:mb-auto">
           <ul className='flex items-center justify-center gap-12 lg:flex-col lg:gap-16'>
             {
               routes.map((route) => (
