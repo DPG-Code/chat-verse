1f1e14b2e9baa908077cfa2889f2f1efe81d6612
Daniel Prieto <prietogaviriadaniel@gmail.com>
2024-01-21 09:58:17 +0100

    feat: new favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <link rel="icon" href="/favicon.png" sizes="any" />
       <body className={outfit.className}>
         <AuthContext>
           <ToasterContext />
